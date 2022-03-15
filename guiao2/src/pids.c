f4b956a39acc48dbd14f47df2aa9e82f8d05fb6b
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2022-03-15 18:38:49 +0100

    guiao 2

--- guiao2/src/pids.c
+++ guiao2/src/pids.c
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void pids(){
     printf("%d is my father's pid and %d is mine.\n", getppid(), getpid());
@@ -143,42 +146,165 @@ bool contains_number_all(int target, int **matrix, uint L, uint C){
     return found;
 }
 
-void matrix_gen(bool all){
+void free_matrix(int** matrix, size_t lines){
+    for(uint i = 0; i < lines; i++)
+        free(matrix[i]);
 
-    const size_t lines = 10, collumns = 10000;
+    free(matrix);
+}
+
+int** matrix_gen(size_t lines, size_t collumns){
 
     int **matrix = calloc(lines, sizeof *matrix);
 
     if(matrix){
 
-        bool do_stuff = true;
+        bool alloc_fail = false;
 
         for(uint i = 0; i < lines; i++)
             if(!(matrix[i] = malloc(sizeof **matrix * collumns)))
-                do_stuff = false;
+                alloc_fail = true;
 
-        if(do_stuff){
+        if(!alloc_fail){
 
             srand((uint) time(NULL));
-
-            const int target = rand() % (ssize_t) collumns;
 
             for(uint i = 0; i < lines; i++)
                 for(uint j = 0; j < collumns; j++)
                     matrix[i][j] = rand() % (ssize_t) collumns;
-
-
-            if(!all)
-                contains_number(target, matrix, lines, collumns);
-            else
-                contains_number_all(target, matrix, lines, collumns);
+        }
+        else{
+            free_matrix(matrix, lines);
+            matrix = NULL;
         }
     }
 
-    for(uint i = 0; i < lines; i++)
-        free(matrix[i]);
+    return matrix;
+}
 
-    free(matrix);
+void matrix_lookup_switch(bool all){
+
+    const size_t lines = 10, collumns = 10000;
+    int** matrix = matrix_gen(lines, collumns);
+    const int target = rand() % (ssize_t) collumns;
+
+
+    if(matrix){
+        if(!all)
+            contains_number(target, matrix, lines, collumns);
+        else
+            contains_number_all(target, matrix, lines, collumns);
+    }
+
+    free_matrix(matrix, lines);
+}
+
+void lookup_in_file(){
+
+    const size_t lines = 10, collumns = 10000;
+    int** matrix = matrix_gen(lines, collumns);
+    const int target = rand() % (ssize_t) collumns;
+
+    const int fd_write = open("resources/matrix.bin", O_WRONLY | O_CREAT | O_TRUNC, 0660);
+
+    if(matrix){
+        if(fd_write > -1){
+
+            for(uint i = 0; i < lines; ++i){
+                ssize_t bread;
+                if((bread = write(fd_write, matrix[i], sizeof *matrix[i] * collumns)) <= 0)
+                    exit(1);
+            }
+
+            close(fd_write);
+
+
+
+            for(uint i = 0; i < lines; ++i){
+
+                if(fork() == 0){
+
+                    const int fd_read = open("resources/matrix.bin", O_RDONLY | O_BINARY);
+
+                    if(fd_read > -1){
+                        lseek(fd_read, i * collumns * sizeof(int), SEEK_SET);
+                        int *p = malloc(sizeof *p);
+
+                        uint j;
+                        for(j = 0; j < collumns; j++){
+                            read(fd_read, p, sizeof *p);
+                            if(*p == target){
+                                free(p);
+                                _exit(1);
+                            }
+                        }
+
+                        free(p);
+                        close(fd_read);
+                        _exit(0); // dont return negative number
+                    }
+                    else
+                        perror("Failed to open file.");
+                }
+            }
+
+            bool found = false;
+
+            for(uint i = 0; i < lines && !found; i++){
+
+                int status = 0;
+                const pid_t pid = wait(&status);
+                const uint exit_code = WEXITSTATUS(status);
+
+                if(exit_code == 1){
+                    found = true;
+                    printf("Child %d found the target %d\n", pid, target);
+                }
+            }
+
+            if(!found)
+                printf("Target %d was not found\n", target);
+
+        }
+        else
+            perror("Failed to open file.");
+
+        free_matrix(matrix, lines);
+    }
+    else
+        perror("Out of memory");
+}
+
+
+
+void bin_to_csv(){
+
+    int *p = malloc(sizeof *p);
+
+    const int read_fd = open("resources/matrix.bin", O_RDONLY);
+    const int write_fd = open("resources/matrix.csv", O_CREAT | O_TRUNC | O_WRONLY, 0660);
+    if(read_fd >= 0 && write_fd >= 0){
+
+        int nl = 0;
+
+        while(read(read_fd, p, sizeof *p) > 0){
+            if(nl++ == 100)
+                write(write_fd, "\n", 1);
+
+            char str[100];
+            snprintf(str, 100, "%d", *p);
+
+            write(write_fd, str, strlen(str));
+
+            write(write_fd, ";", 1);
+        }
+
+        close(write_fd);
+        close(read_fd);
+    }
+
+
+    free(p);
 }
 
 int main(int argc, const char** argv){
@@ -204,20 +330,28 @@ int main(int argc, const char** argv){
             break;
 
         case 5:
-            matrix_gen(false);
+            matrix_lookup_switch(false);
             break;
 
         case 6:
-            matrix_gen(true);
+            matrix_lookup_switch(true);
+            break;
+
+        case 7:
+            lookup_in_file();
+            break;
+
+        case 8:
+            bin_to_csv();
             break;
 
         default:
-            printf("Invalid option.\n");
+            perror("Invalid option.\n");
             break;
         }
     }
     else
-        printf("No option provided.\n");
+        perror("No option provided.\n");
 
 
     return 0;
